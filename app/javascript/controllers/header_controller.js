18426309f785cf16c65067a2d5587efe0b10f2f6
Jorge Luis <jorgeluis.jimenez@xmartlabs.com>
2023-01-13 11:01:38 -0300

    create modal controller

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,10 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 import { toggle } from 'el-transition'
 
 export default class extends Controller {
-    static targets = ['openUserMenu'];
+    static targets = ['openUserMenu', 'userAuthLink'];
 
     connect() {
         this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu);
+
+        this.userAuthLinkTargets.forEach((link) => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault()
+                document.getElementById('modal-trigger').click()
+            })
+        })
     }
 
     toggleDropdownMenu() {
